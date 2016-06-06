a4d40c7a87e79cf0b85ce7695c9307b777b5e5dc
yushi <tachikomano007@gmail.com>
2016-06-06 08:09:28 +0000

    update userpage/itempage

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :logged_in_user , except: [:show]
   before_action :set_item, only: [:show]
+  before_action :set_user, only: [:show]
 
   def new
     if params[:q]
@@ -13,10 +14,16 @@ class ItemsController < ApplicationController
   end
 
   def show
+    @have = @item.have_users
+    @want = @item.want_users
   end
 
   private
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
